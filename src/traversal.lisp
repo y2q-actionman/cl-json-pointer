5d3a3d12c119126b4432722e7aa1939d72028036
y2q-actionman <y2q-actionman@users.noreply.github.com>
2018-09-20 17:09:05 +0900

    cleanup traversal.lisp

--- src/traversal.lisp
+++ src/traversal.lisp
@@ -1,5 +1,70 @@
 (in-package :cl-json-pointer)
 
+(defconstant +delete-request+
+  '+delete-request+
+  "A special value indicates deletion, used by setters.")
+
+;;; Switches
+
+(defparameter *traverse-treat-string-as-atom* t
+  ;; I don't want to treat string as an array.
+  "If this is T, cl-json-pointer trests string as atom.")
+
+(defparameter *traverse-consider-plist* nil
+  ;; I think there is no way to define good `plist-like-p', because plist
+  ;; does not restricted. Whether its keys are compared by `eq'
+  ;; (http://www.lispworks.com/documentation/HyperSpec/Body/f_eq.htm),
+  ;; I think I should not assume the keys are always a symbol.
+  "If this is T, cl-json-pointer considers plists at traversaling.")
+
+(defparameter *traverse-nil-set-to-last-method* :list
+  "Determines how to set to the last (by '-') of NIL.
+- `:list' :: pushes <value> as an ordinal list.
+- `:alist' :: pushes (reference-token . <value>) as an alist.
+- `:plist' :: appends (reference-token <value>) as an plist.
+- `:array' :: makes a new array contains <value>.
+")
+
+(defparameter *traverse-nil-set-to-index-method* :error
+  "Determines how to set to NIL by an index.
+- `:error' :: throws an error.
+- `:alist' :: pushes (reference-token . <value>) as an alist.
+- `:plist' :: appends (reference-token <value>) as an plist.
+")
+
+(defparameter *traverse-nil-set-to-name-method* :alist
+  "Determines how to set to NIL by a name.
+- `:alist' :: pushes (reference-token . <value>) as an alist.
+- `:plist' :: appends (reference-token <value>) as an plist.
+")
+
+(defparameter *traverse-non-adjustable-array-set-to-last-method* :create
+  "Determines how to set to the last (by '-') of non-adjutable arrays.
+- `:error' :: throws an error.
+- `:create' :: makes a new adjustable array contains <value>.
+")
+
+(defparameter *traverse-array-delete-method* nil
+  "Determines how to delete an value of arrays.
+- `nil' :: fills nil.
+- `:error' :: throws an error.
+")
+
+;;; Tools
+
+(defun alist-like-p (list)
+  (every #'consp list))
+
+(defun plist-like-p (list)
+  (loop for (k nil) on list by #'cddr
+     always (symbolp k)))
+
+(defun compare-string-by-readtable-case (a b &optional (case (readtable-case *readtable*)))
+  ;; TODO: should I use `ignore-errors' for alist (or plist) ?
+  (ecase case
+    ((:upcase :downcase) (string-equal a b))
+    ((:preserve :invert) (string= a b))))
+
 (defmacro thunk-lambda (&body form)
   "Used for making thunks."
   (with-gensyms (_)
@@ -7,8 +72,7 @@
        (declare (ignore ,_))
        ,@form)))
 
-(defmacro chained-setter-lambda ((var) (target next-function)
-				 &body case-clauses)
+(defmacro chained-setter-lambda ((var) (target next-function) &body case-clauses)
   `(lambda (,var)
      (declare (ignorable ,var))
      (funcall ,next-function
@@ -16,25 +80,29 @@
 		       ,@case-clauses)
 		     ,target))))
 
+;;; Reference Token
 
-(defconstant +delete-request+
-  '+delete-request+)
-
-(defun read-reference-token-as-index (reference-token)
+(defun read-reference-token-as-index (reference-token &optional (errorp t))
   (cond ((eq reference-token +last-nonexistent-element+)
 	 +last-nonexistent-element+)
 	((and (> (length reference-token) 1)
 	      (char= (char reference-token 0) #\0))
-	 (error 'json-pointer-bad-index-error
-		:format-control "reference token (~A) should not start with 0 as index"
-		:format-arguments (list reference-token)))
+	 (when errorp
+	   (error 'json-pointer-bad-index-error
+		  :format-control "reference token (~A) should not start with 0 as index"
+		  :format-arguments (list reference-token)))
+	 nil)
 	(t
 	 (handler-case (parse-integer reference-token)
 	   (error ()
-	     (error 'json-pointer-bad-index-error
-		    :format-control "reference token (~A) cannot be read as index"
-		    :format-arguments (list reference-token)))))))
+	     (when errorp
+	       (error 'json-pointer-bad-index-error
+		      :format-control "reference token (~A) cannot be read as index"
+		      :format-arguments (list reference-token)))
+	     nil)))))
 
+
+;;; Main traversal.
 
 (defgeneric traverse-by-reference-token (obj rtoken parental-setter)
   (:documentation "Traverses an object with a reference token, and
@@ -51,36 +119,14 @@
 
 (defmethod traverse-by-reference-token (obj rtoken parental-setter)
   (declare (ignore parental-setter))
-  ;; bottom case 1 -- refers an unsupported type object.
+  ;; bottom case -- refers an unsupported type object.
   (error-on-traversing-atom obj rtoken))
-
-(defmethod traverse-by-reference-token (obj (rtoken null) parental-setter)
-  ;; bottom case 2 -- refers an object with an empty token.
-  (values obj obj
-	  (if parental-setter
-	      (thunk-lambda
-		(error 'json-pointer-access-error
-		       :format-control "setting with an empty reference token is not supported (object is ~A)"
-		       :format-arguments (list obj))))))
-
-
-(defparameter *traverse-treat-string-as-atom* t
-  ;; I don't want to treat string as an array.
-  "If this is T, cl-json-pointer trests string as atom.")
 
 (defmethod traverse-by-reference-token ((obj string) rtoken parental-setter)
   (declare (ignore parental-setter))
   (if *traverse-treat-string-as-atom*
       (error-on-traversing-atom obj rtoken)
       (call-next-method)))
-
-
-(defun compare-string-by-case (a b &optional (case (readtable-case *readtable*)))
-  ;; TODO: should I use `ignore-errors' for alist (or plist) ?
-  (ecase case
-    ((:upcase :downcase) (string-equal a b))
-    ((:preserve :invert) (string= a b))))
-
 
 (defun bad-deleter-error (obj rtoken)
   (error 'json-pointer-not-found-error
@@ -89,7 +135,7 @@
 
 (defun traverse-alist-by-reference-token (alist rtoken parental-setter)
   ;; accepts `nil' as alist.
-  (if-let ((entry (assoc rtoken alist :test #'compare-string-by-case)))
+  (if-let ((entry (assoc rtoken alist :test #'compare-string-by-readtable-case)))
     (values (cdr entry) entry
 	    (if parental-setter
 		(chained-setter-lambda (x) (alist parental-setter)
@@ -105,7 +151,7 @@
   ;; accepts `nil' as plist.
   (loop for plist-head on plist by #'cddr
      as (k v) = plist-head
-     when (compare-string-by-case k rtoken) ; plist often uses `eq', but we use `string='.
+     when (compare-string-by-readtable-case k rtoken) ; plist often uses `eq', but I use this.
      return (values v plist-head
 		    (if parental-setter
 			(chained-setter-lambda (x) (plist parental-setter)
@@ -139,63 +185,25 @@
 			       :format-control "Index ~A is out-of-index from ~A"
 			       :format-arguments (list index list)))))))))
 
-
-(defun alist-like-p (list)
-  (every #'consp list))
-
-(defun plist-like-p (list)
-  (loop for (k nil) on list by #'cddr
-     always (symbolp k)))
-
-(defparameter *traverse-consider-plist* nil
-  ;; I think there is no way to define good `plist-like-p', because plist
-  ;; does not restricted. Whether its keys are compared by `eq'
-  ;; (http://www.lispworks.com/documentation/HyperSpec/Body/f_eq.htm),
-  ;; I think I should not assume the keys are always a symbol.
-  "If this is T, cl-json-pointer considers plists at traversaling.")
-
 (defmethod traverse-by-reference-token ((obj list) rtoken parental-setter)
-  (if (ignore-errors
-	(read-reference-token-as-index rtoken))
-    ;; rtoken is ambiguous with index.
-    (cond ((alist-like-p obj)
-	   (traverse-alist-by-reference-token obj rtoken parental-setter))
-	  ((and *traverse-consider-plist*
-		(plist-like-p obj))
-	   (traverse-plist-by-reference-token obj rtoken parental-setter))
-	  (t
-	   (traverse-ordinal-list-by-reference-token obj rtoken parental-setter)))
-    ;; I assume `rtoken' is not index, so considers alist (or plist).
-    (if (and *traverse-consider-plist*
-	     (plist-like-p obj))
-	(traverse-plist-by-reference-token obj rtoken parental-setter)
-	(traverse-alist-by-reference-token obj rtoken parental-setter))))
-
-(defparameter *traverse-nil-set-to-last-method* :list
-  "Determines how to set to the last (by '-') of NIL.
-- :list :: pushes <value> as an ordinal list.
-- :alist :: pushes (reference-token . <value>) as an alist.
-- :plist :: appends (reference-token <value>) as an plist.
-- :array :: makes a new array contains <value>.
-")
-
-(defparameter *traverse-nil-set-to-index-method* :error
-  "Determines how to set to NIL by an index.
-- :error :: throws an error.
-- :alist :: pushes (reference-token . <value>) as an alist.
-- :plist :: appends (reference-token <value>) as an plist.
-")
-
-(defparameter *traverse-nil-set-to-name-method* :alist
-  "Determines how to set to NIL by a name.
-- :alist :: pushes (reference-token . <value>) as an alist.
-- :plist :: appends (reference-token <value>) as an plist.
-")
+  (if (read-reference-token-as-index rtoken nil)
+      ;; rtoken is ambiguous with index.
+      (cond ((alist-like-p obj)
+	     (traverse-alist-by-reference-token obj rtoken parental-setter))
+	    ((and *traverse-consider-plist*
+		  (plist-like-p obj))
+	     (traverse-plist-by-reference-token obj rtoken parental-setter))
+	    (t
+	     (traverse-ordinal-list-by-reference-token obj rtoken parental-setter)))
+      ;; I assume `rtoken' is not index, so considers alist (or plist).
+      (if (and *traverse-consider-plist*
+	       (plist-like-p obj))
+	  (traverse-plist-by-reference-token obj rtoken parental-setter)
+	  (traverse-alist-by-reference-token obj rtoken parental-setter))))
 
 (defmethod traverse-by-reference-token ((obj null) rtoken parental-setter)
   ;; empty. this is problematic for setting.
-  (let* ((index (ignore-errors
-		  (read-reference-token-as-index rtoken)))
+  (let* ((index (read-reference-token-as-index rtoken nil))
 	 (setter-method
 	  (cond ((eq index +last-nonexistent-element+)
 		 *traverse-nil-set-to-last-method*)
@@ -223,12 +231,10 @@
 			    :format-control "Set to nil by index is not supported"))))))))
     (values nil nil setter)))
 
-(defmethod traverse-by-reference-token ((obj standard-object) rtoken parental-setter)
-  ;; cl-json:fluid-object can be treated here.
-  (let* ((class (class-of obj))
-	 (slot (find rtoken (class-slots class)
-		     :key #'slot-definition-name
-		     :test #'compare-string-by-case)))
+(defun traverse-by-reference-token-using-class (obj rtoken parental-setter class)
+  (let ((slot (find rtoken (class-slots class)
+		    :key #'slot-definition-name
+		    :test #'compare-string-by-readtable-case)))
     (if slot
 	(let ((bound? (slot-boundp-using-class class obj slot)))
 	  (values (if bound?
@@ -242,14 +248,18 @@
 		(if parental-setter
 		    (thunk-lambda
 		      (error 'json-pointer-access-error
-				:format-control "object ~A does not have '~A' slot"
-				:format-arguments (list obj rtoken))))))
-    ;; TODO: support structure-object?
-    ;; TODO: support condition-object?
-    ))
+			     :format-control "object ~A does not have '~A' slot"
+			     :format-arguments (list obj rtoken))))))))
+
+(defmethod traverse-by-reference-token ((obj standard-object) rtoken parental-setter)
+  ;; cl-json:fluid-object can be treated here.
+  (traverse-by-reference-token-using-class obj rtoken parental-setter (class-of obj)))
+
+(defmethod traverse-by-reference-token ((obj structure-object) rtoken parental-setter)
+  (traverse-by-reference-token-using-class obj rtoken parental-setter (class-of obj)))
 
 (defmethod traverse-by-reference-token ((obj hash-table) rtoken parental-setter)
-  ;; TODO: use `compare-string-by-case'? (depending on json lib..)
+  ;; TODO: use `compare-string-by-readtable-case' (depending on json lib..)
   (multiple-value-bind (value exists?)
       (gethash rtoken obj)
     (values value
@@ -258,12 +268,6 @@
 		(chained-setter-lambda (x) (obj parental-setter)
 		  (+delete-request+ (remhash rtoken obj))
 		  (otherwise (setf (gethash rtoken obj) x)))))))
-
-(defparameter *traverse-non-adjustable-array-set-to-last-method* :create
-  "Determines how to set to the last (by '-') of non-adjutable arrays.
-- :error :: throws an error.
-- :create :: makes a new adjustable array contains <value>.
-")
 
 (defun make-array-tail-adder (array parental-setter
 			      &key (set-to-last-method *traverse-non-adjustable-array-set-to-last-method*))
@@ -304,26 +308,30 @@
 		   (if parental-setter
 		       (thunk-lambda
 			 (error 'json-pointer-not-found-error
-				   :format-control "Index ~A (pointer ~A) is out-of-index from ~A"
-				   :format-arguments (list index rtoken obj))))))
+				:format-control "Index ~A (pointer ~A) is out-of-index from ~A"
+				:format-arguments (list index rtoken obj))))))
 	  (t
 	   (values (aref obj index) index
 		   (if parental-setter
 		       (chained-setter-lambda (x) (obj parental-setter)
-			 (+delete-request+ (error "under implementation -- array remove"))
+			 (+delete-request+
+			  (ecase *traverse-array-delete-method*
+			    ((nil) (setf (aref obj index) x))
+			    (:error
+			     (error 'json-pointer-access-error
+				    :format-control "Delete from array is error (array ~A, index ~A)"
+				    :format-arguments (list obj index)))))
 			 (otherwise (setf (aref obj index) x)))))))))
 
 (defun traverse-by-json-pointer (obj parsed-pointer make-setter?)
-  "Traverses an object with a parsed json-pointer, and returns three
-values: a referred object, existence (boolean), and a closure can be
-used as a setter."
+  "Traverses an object with a parsed json-pointer, and returns three values:
+the referred object, existence (boolean), and a closure can be used as a setter."
   (let ((value obj)
 	(exists? t)
 	(setter
 	 (if make-setter?
 	     (lambda (x) (setf obj x)))))
-    (loop for (rtoken . next) on parsed-pointer
+    (loop for rtoken in parsed-pointer
        do (setf (values value exists? setter)
-		(traverse-by-reference-token value rtoken setter))
-       while next)
+		(traverse-by-reference-token value rtoken setter)))
     (values value exists? setter)))
